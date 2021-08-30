39fa284c3b48624ca4e66bf0ec8b286cbb58dabb
mansi277 <bhangadia_mansi.cs@ghrce.raisoni.net>
2021-08-30 18:58:10 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'practice';
+  currentVal='';
+  myEvent(evt:String){
+    console.log(evt)
+    this.currentVal=<string>evt
+  }
+  username='mansi';
+  disabledbox=true;
+  enablebox(){
+    this.disabledbox=false;
+  }
+  show=false
+  color='blue';
+  data=['mansi','arya','dheer']
+  dataobj=[
+    {
+      namee:'mansi',
+      age:20
+    },
+    {
+      namee:'manu',
+      age:24
+    },
+    {
+      namee:'maanvi',
+      age:29
+    }
+  ]
 }
